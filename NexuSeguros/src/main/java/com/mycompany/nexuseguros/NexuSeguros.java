a9fb4dadf4580b88a98bf0cd9a9b8e7aee58b990
Cesar-Galahad <conejo_guevara@hotmail.com>
2025-07-01 11:27:32 -0600

    prueba

---
+++ NexuSeguros/src/main/java/com/mycompany/nexuseguros/NexuSeguros.java
@@ -0,0 +1,23 @@
+package com.mycompany.nexuseguros;
+import com.login.bienvenidaAC;
+import com.login.login;
+
+
+/**
+ *
+ * @author Uroboros
+ */
+public class NexuSeguros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            // Reemplaza NombreDeTuJFrame por el nombre real del JFrame Form
+            new bienvenidaAC().setVisible(true);
+            //new login().setVisible(true); 
+            //new dashboardAgente().setVisible(true);
+            //new dashboardCliente().setVisible(true);
+            
+        });
+    }
+}
